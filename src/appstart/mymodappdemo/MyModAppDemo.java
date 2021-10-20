60eb4db374a3eb7881a784c7227fddb662170d4a
ThundePro <steamus500@gmail.com>
2021-10-20 19:35:26 +0300

    Initial commit

---
+++ src/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,16 @@
+package appstart.mymodappdemo;
+import appfuncs.simplefuns.SimpleMathFuncs;
+
+public class MyModAppDemo {
+    public static void main(String[] args) {
+        if (SimpleMathFuncs.isFactor(2,10))
+            System.out.println("2 - множитель 10");
+        System.out.println("Наименьший общий множитель "
+                + "35 и 105 - "
+                + SimpleMathFuncs.lcf(35, 105));
+
+        System.out.println("Наибольший общий множитель "
+                + "35 и 105 - "
+                + SimpleMathFuncs.gcf(35, 105));
+    }
+}
